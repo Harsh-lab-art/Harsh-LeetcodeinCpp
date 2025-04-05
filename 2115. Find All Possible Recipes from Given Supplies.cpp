4ed5df77c4613fc1e7efc38cb470e06c099799c4
Harsh Kumar Verma <harsh9760verma@gmail.com>
2025-04-05 23:30:19 +0530

    2115. Find All Possible Recipes from Given Supplies.cpp

---
+++ 2115. Find All Possible Recipes from Given Supplies.cpp
@@ -0,0 +1,40 @@
+class Solution 
+{
+    public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) 
+    {
+        unordered_map<string,vector<string>>adj;
+        unordered_map<string,int>indegree;
+        unordered_set<string> supply(supplies.begin(),supplies.end());
+        for(int i=0;i<recipes.size();++i)
+        {
+            indegree[recipes[i]]=0;
+            for(string& ingredient : ingredients[i])
+            {
+                if(supply.count(ingredient)==0)
+                {
+                    adj[ingredient].push_back(recipes[i]);
+                    indegree[recipes[i]]++;
+                }
+            }
+        }
+        queue<string> q;
+        for(auto&[recipe,indeg]: indegree)
+        if(indeg==0)
+        q.push(recipe);
+        vector<string> res;
+        while(!q.empty())
+        {
+            string curr=q.front();
+            q.pop();
+            res.push_back(curr);
+            for(string & next : adj[curr])
+            {
+                indegree[next]--;
+                if(indegree[next]==0)
+                q.push(next);
+            }
+        }
+        return res;
+    }
+};
