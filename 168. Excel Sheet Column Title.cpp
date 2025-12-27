b9ace9711be3723b7ed05609774295e9eba7baf2
Harsh Kumar Verma <harsh9760verma@gmail.com>
2025-12-27 23:31:00 +0530

    Implement Excel column title conversion

---
+++ 168. Excel Sheet Column Title.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+    public:
+    string convertToTitle(int columnNumber) 
+    {
+        string ans="";
+        while(columnNumber>0)
+        {
+            columnNumber--;
+            char ch=(char)('A'+columnNumber%26);
+            ans=ch+ans;
+            columnNumber/=26;
+        }
+        return ans;
+    }
+};
